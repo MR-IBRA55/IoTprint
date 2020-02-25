d926190cc16b33e01c208f20fe4819ddeb88bba5
CIR <52345388+Captain-IR@users.noreply.github.com>
2020-02-25 23:44:23 +0200

    Adding Create Order Button

--- client/app/static/js/myFiles.js
+++ client/app/static/js/myFiles.js
@@ -11,30 +11,46 @@ function fetchSketches() {
         for (var sketch in sketches) {
           output +=
             `
-              <table id=${sketches[sketch]._id} class="myFilesTable">
+              <table class="myFilesTable">
                 <tr>
                   <th>${sketches[sketch].display_name}</th>
-                  <td><button class="btnOrderNow" type="button">Order</button></td>
+                  <td><button id=${sketches[sketch]._id} class="BtnsOrder" type="button">Order</button></td>
                 </tr>
               </table>
             `
           const asideDynamic = document.getElementById('asideDynamic');
           asideDynamic.innerHTML = output
         }
+        orderNow();
       } else {
         alert(JSON.parse(this.response).msg)
       }
     }
     xhr.send();
-    orderNow();
   })
 
 }
 
 function orderNow() {
+  var btnsOrder = document.getElementsByClassName("BtnsOrder");
+  for (var i = 0; i < btnsOrder.length; i++) {
+    btnsOrder[i].addEventListener('click', function (e) {
+      var data = JSON.stringify({ "sketch": e.target.id })
+      var xhr = new XMLHttpRequest();
+      xhr.withCredentials = true;
+      xhr.open('POST', `http://${document.domain}/api/order`, true);
 
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.setRequestHeader("User", "5e54ed3692d6474676eb19a0");
+
+      xhr.onload = function () {
+        alert(JSON.parse(this.response).msg)
+      }
+
+      xhr.send(data);
+    })
+  }
 }
 
 
 fetchSketches();
-
